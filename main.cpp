0ac3591b6b99af85e06e9ce16dd04b8c4b81b05b
DevAndreww <andrea.sette2@marconibari.edu.it>
2025-11-20 08:47:44 +0000

    consegna

--- main.cpp
+++ main.cpp
@@ -1,11 +1,38 @@
 #include <iostream>
 using namespace std;
 
-int main() {
-    string nome;
-    cout << "Inserisci il tuo nome: ";
-    cin >> nome;
-    cout << "Ciao " <<nome<< "!" << endl;
+int massimo(int n1, int n2)
+{
+   int max;
+   
+   if(n1>n2)
+   {
+       max=n1;
+   }
+   else
+   {
+       max=n2;
+   }
+
+    return max;
 }
 
-//LEGGERE LE ISTRUZIONI NEL FILE README.md
+int main()
+
+{
+    int n1, n2, n3, n4, max;
+    
+    cout << "Inserisci 4 valori:" << endl;
+    
+    cin >> n1 >> n2 >> n3 >> n4;
+    
+    max = massimo(n1,n2);
+    
+    max = massimo(n2,n3);
+    
+    max = massimo (max,n4);
+    
+    cout << "Il valore massimo è " << max << endl;
+    
+    return 0;
+}
